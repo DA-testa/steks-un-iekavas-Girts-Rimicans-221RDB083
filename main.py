d32039032380c878dd4c60431d3f3b42969c19e8
Girts Rimicans <girts.rimicans@tietoevry.com>
2023-02-07 08:52:37 +0200

    print next bracket

--- main.py
+++ main.py
@@ -20,21 +20,30 @@ def find_mismatch(text):
             opening_brackets_stack.append(Bracket(next, i))
             pass
 
+    bad_pos = []
     length = len(opening_brackets_stack) - 1
     for i in range(0, length):
-        if i > opening_brackets_stack[length - i][1]:
-            break
+        if len(bad_pos):
+            if are_matching(opening_brackets_stack[bad_pos[0]][0], opening_brackets_stack[bad_pos[1]][0]) is True:
+                return opening_brackets_stack[bad_pos[1]][1] if opening_brackets_stack[bad_pos[1]][0] not in ")]}" else opening_brackets_stack[bad_pos[1]+1][1]
+            return opening_brackets_stack[bad_pos[1]][1] if opening_brackets_stack[bad_pos[1]][0] in ")]}" else opening_brackets_stack[bad_pos[1]+1][1]
+        else:
+            if i > opening_brackets_stack[length - i][1]:
+                break
 
-        if are_matching(opening_brackets_stack[i][0], opening_brackets_stack[length - i][0]) is not True:
-            if are_matching(opening_brackets_stack[i][0], opening_brackets_stack[i + 1][0]) is True:
-                continue
-            return opening_brackets_stack[i][1] if opening_brackets_stack[i][0] not in ")]}" else opening_brackets_stack[i+1][1]
-    
+            if are_matching(opening_brackets_stack[i][0], opening_brackets_stack[length - i][0]) is not True:
+                if are_matching(opening_brackets_stack[i][0], opening_brackets_stack[i + 1][0]) is True:
+                    continue
+                bad_pos = [i, length - 1]
+                if i == (length - 1):
+                    return opening_brackets_stack[i][1] if opening_brackets_stack[i][0] not in ")]}" else opening_brackets_stack[i+1][1]
+
     return "Success"
 
 
 def main():
-    text = input()
+    # text = input()
+    text = "[({])}"
     mismatch = find_mismatch(text)
     print(mismatch)
 
