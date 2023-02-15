920975854982092fbea58f96a0d044d29fc40cbb
Girts Rimicans <girts.rimicans@tietoevry.com>
2023-02-15 18:41:14 +0200

    optimize3

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    bracket = ""
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i + 1))
-            pass
 
         if next in ")]}":
             if len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[0][0], next) is False:
